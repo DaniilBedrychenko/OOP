a7e410161ac091031d102b17d5dbe895f1f971f9
DaniilBedrychenko <bedrychenko787@gmail.com>
2025-10-20 17:07:45 +0300

    Implement Queue class with basic operations

---
+++ HW11.2/Queue.cpp
@@ -0,0 +1,86 @@
+#include "Queue.h"
+#include <iostream>
+using namespace std;
+
+Queue::Queue(int size)
+{
+    this->size = size;
+    arr = new int[size];
+    current_size = 0;
+}
+
+Queue::Queue(const Queue& other)
+{
+    this->size = other.size;
+    arr = new int[other.size];
+    for (int i = 0; i < other.current_size; i++) {
+        arr[i] = other.arr[i];
+    }
+    current_size = other.current_size;
+}
+
+Queue& Queue::operator=(const Queue& other)
+{
+    if (this == &other) return *this;
+    if (arr != nullptr) delete[] arr;
+    this->size = other.size;
+    arr = new int[other.size];
+    for (int i = 0; i < other.current_size; i++) {
+        arr[i] = other.arr[i];
+    }
+    current_size = other.current_size;
+    return *this;
+}
+
+Queue::~Queue()
+{
+    delete[] arr;
+}
+
+void Queue::Print()
+{
+    cout << "Current size: " << current_size << endl;
+    for (int i = 0; i < current_size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void Queue::Clear()
+{
+    current_size = 0;
+}
+
+int Queue::Dequeue()
+{
+    if (IsEmpty()) return 0;
+    int result = arr[0];
+    for (int i = 0; i < current_size - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    current_size--;
+    return result;
+}
+
+void Queue::Enqueue(int value)
+{
+    if (current_size == size) {
+        size *= 2;
+        int* copy = new int[size];
+        for (int i = 0; i < current_size; i++) {
+            copy[i] = arr[i];
+        }
+        delete[] arr;
+        arr = copy;
+    }
+    arr[current_size++] = value;
+}
+
+bool Queue::IsEmpty()
+{
+    return current_size == 0;
+}
+
+int Queue::getSize()
+{
+    return current_size;
+}
